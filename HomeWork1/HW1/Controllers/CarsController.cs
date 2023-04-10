3b0ab07e15cc2f2f7ea1bd3b37c71f50f7e71686
alexkoste <34475394+alexkoste@users.noreply.github.com>
2023-04-10 12:06:02 +0300

    HomeWork1
    HomeWork1

---
+++ HomeWork1/HW1/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ILogger<CarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Car> Get()
+        {
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car("Ford", "Focus", 2011, "USA"));
+            cars.Add(new Car("Mazda", "Cx-5", 2011, "Japan"));
+            cars.Add(new Car("KIA", "CEED", 2009, "Korea"));
+
+            return cars;
+        }
+    }
+}
---
+++ HomeWork1/HW1/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace HW1.Models
+{
+    public class Car
+    {
+        public Car(string make, string model, int year, string country)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Country = country;
+        }
+
+        public string Model { get; set; }
+
+        public string Make { get; set; }
+
+        public int Year { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
