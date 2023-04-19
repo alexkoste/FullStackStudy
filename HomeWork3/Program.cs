0622fd6dbcb20f244b69454f9e6632d4f28e4064
alexkoste <34475394+alexkoste@users.noreply.github.com>
2023-04-19 11:12:35 +0300

    HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        public delegate void Prnt(string text);
+
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Prnt print;
+
+            print = PrintGreen;
+            print += PrintYellow;
+
+            print(message);
+        }
+
+        private static void PrintGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+        }
+
+        private static void PrintYellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+        }
+
+    }
+}
